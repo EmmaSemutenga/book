8049ce67fca5624b751504abd1639c9e8b617b9e
Emmanuel Semutenga <emzsema@gmail.com>
2021-08-03 20:03:49 +0300

    added test runner to settings

--- my_project/settings.py
+++ my_project/settings.py
@@ -135,4 +135,4 @@ STATICFILES_DIRS = [BASE_DIR / 'todosite/static']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(locals())
+django_heroku.settings(locals(), test_runner=False)
